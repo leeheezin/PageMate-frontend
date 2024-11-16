d04071cc553410f578ab94172fbce11c0506d30c
EunseoKim <dmstj9408@gmail.com>
2024-11-17 00:58:01 +0900

    [feature]navbar, footer 완성

--- src/common/component/Footer.tsx
+++ src/common/component/Footer.tsx
@@ -11,31 +11,47 @@ const Footer: React.FC = () => {
   let navigate = useNavigate();
 
   return (
-    <div className="sidebar">
+    <>
+      {/* desktop 사이드바 footer */}
+      <div className="sidebar">
       {/* 상단 버튼들 */}
-      <div className="top-buttons">
-      <button className="icon-button">
-        <img onClick={() => navigate("/")}  src={iconHome} alt="Home" />
-      </button>
-      <button className="icon-button">
-        <img onClick={() => navigate("/search")} src={iconSearch} alt="Search" />
-      </button>
-      <button className="icon-button">
-        <img onClick={() => navigate("/post/write")}src={iconPostAdd} alt="Add Post" />
-      </button>
-      <button className="icon-button">
-        <img onClick={() => navigate("/mypage")} src={iconMypage} alt="My Page" />
-      </button>
-    </div>
+        <div className="top-buttons">
+          <button className="icon-button" onClick={() => navigate("/")}>
+            <img src={iconHome} alt="Home" />
+          </button>
+          <button className="icon-button" onClick={() => navigate("/search")}>
+            <img src={iconSearch} alt="Search" />
+          </button>
+          <button className="icon-button" onClick={() => navigate("/post/write")}>
+            <img src={iconPostAdd} alt="Add Post" />
+          </button>
+          <button className="icon-button" onClick={() => navigate("/mypage")}>
+            <img src={iconMypage} alt="My Page" />
+          </button>
+        </div>
 
-    {/* 하단 햄버거 버튼 */}
-    <div className="bottom-button">
-      <button className="icon-button">
-        <img src={iconMenu} alt="Menu"/>
-      </button>
-    </div>
-  </div>  
-  )
-}
+        {/* 하단 햄버거 버튼 */}
+        <div className="bottom-button">
+          <button className="icon-button">
+            <img src={iconMenu} alt="Menu" />
+          </button>
+        </div>
+      </div>
+
+      {/* mobile 하단 footer */}
+      <div className="mobile-footer">
+        <button className="mobile-icon-button" onClick={() => navigate("/")}>
+          <img src={iconHome} alt="Home" />
+        </button>
+        <button className="mobile-icon-button" onClick={() => navigate("/post/write")}>
+          <img src={iconPostAdd} alt="Add Post" />
+        </button>
+        <button className="mobile-icon-button" onClick={() => navigate("/mypage")}>
+          <img src={iconMypage} alt="My Page" />
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Footer;
