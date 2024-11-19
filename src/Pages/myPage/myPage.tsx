8fef3f1a12c1647d5294b047463c4419f504455b
ideal402 <sksekdh@gmail.com>
2024-11-19 16:43:00 +0900

    [css] 모바일 버전 사이드 마진 수정

--- src/Pages/myPage/myPage.tsx
+++ src/Pages/myPage/myPage.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    padding: 20px;
+`;
+const MyPageArea = styled.div`
+  margin-top: 60px;
+  min-height: calc(100vh - 60px);
+  width: 960px;
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  align-items: center;    
+  -ms-overflow-style: none; /* IE, Edge */
+  scrollbar-width: none; /* Firefox */
+
+  &::-webkit-scrollbar {
+    display: none; /* Chrome, Safari */
+  }
+`;
+
+const ProfileArea = styled.div`
+  width: 100%;
+  height: 220px;
+  border: 1px solid black;
+`;
+const PostArea = styled.div`
+  width: 100%;
+`;
 const MyPage: React.FC = () => {
-    return (
-        <div>MyPage</div>
-    );
+  return (
+    <Container>
+      <MyPageArea>
+        <ProfileArea>asdf</ProfileArea>
+        <PostArea>asdf</PostArea>
+      </MyPageArea>
+    </Container>
+  );
 };
 
 export default MyPage;
