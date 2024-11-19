f3b93135fd3c5b5d9b2691ffb25becb64e34b71d
ideal402 <sksekdh@gmail.com>
2024-11-19 19:08:29 +0900

    [css] 스타일 수정

---
+++ src/Pages/myPage/component/post.tsx
@@ -0,0 +1,161 @@
+import styled from "styled-components";
+import LikeButton from "../../../components/likeBtn";
+import CommentButton from "../../../components/comBtn";
+import Comment from "../../../components/comment";
+import { useEffect, useState } from "react";
+
+interface Comment {
+    author: string;
+    text: string;
+  }
+  
+  interface PostProps {
+    post: {
+      bookTitle: string;
+      bookAuthor: string;
+      title: string;
+      text: string;
+      date: string;
+      author: string;
+      profilePhoto: string;
+      likes: number;
+      comments: Comment[];
+    };
+  }
+  
+
+const StyledPost = styled.div`
+  width: 100%;
+  max-width: 846px;
+  border: 1px solid #878787;
+  box-shadow: 0 4px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+  border-radius: 16px;
+  margin: auto;
+  margin-bottom: 28px;
+  padding: 16px;
+
+  @media (max-width: 480px) {
+    max-width: 330px;
+    padding: 8px;
+  }
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 8px;
+`;
+
+const ProfileInfo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ProfilePhoto = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  margin-right: 8px;
+`;
+
+const Name = styled.span`
+  font-weight: bold;
+  font-size: 16px;
+`;
+
+const TitleDate = styled.div``;
+
+const Date = styled.div`
+  font-size: 12px;
+  color: #666;
+`;
+
+const Title = styled.h4`
+  text-align: left;
+  margin: 0;
+`;
+
+const Content = styled.p`
+  color: #014421;
+  font-size: 14px;
+  margin-bottom: 16px;
+`;
+
+const Footer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 5px;
+  margin-bottom: 8px;
+`;
+const Inner = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+const CommentSectionContainer = styled.div`
+  width: 100%;
+  margin-top: 8px;
+`;
+const BookTit = styled.div``;
+const BTitle = styled.div``;
+const BAuthor = styled.div`
+  color: #a4a4a4;
+`;
+const Post: React.FC<PostProps> = ({ post }) => {
+    const {
+      bookTitle,
+      bookAuthor,
+      title,
+      text,
+      date,
+      author,
+      profilePhoto,
+      likes,
+      comments,
+    } = post;
+  
+    const [commentsVisible, setCommentsVisible] = useState(false);
+  
+    const toggleCommentsVisibility = () => {
+      setCommentsVisible(!commentsVisible);
+    };
+  
+    return (
+      <StyledPost>
+        <Header>
+          <TitleDate>
+            <Title>{title}</Title>
+            <Date>{date}</Date>
+          </TitleDate>
+          <ProfileInfo>
+            <ProfilePhoto src={profilePhoto} alt="Profile" />
+            <Name>{author}</Name>
+          </ProfileInfo>
+        </Header>
+        <Content>{text}</Content>
+        <Footer>
+          <Inner>
+            <LikeButton count={likes} />
+            <CommentButton
+              count={comments.length}
+              onClick={toggleCommentsVisibility}
+            />
+          </Inner>
+          <BookTit>
+            <BTitle>{bookTitle}</BTitle>
+            <BAuthor>{bookAuthor}</BAuthor>
+          </BookTit>
+        </Footer>
+        {commentsVisible && (
+          <CommentSectionContainer>
+            <Comment comments={comments} visible={commentsVisible} />
+          </CommentSectionContainer>
+        )}
+      </StyledPost>
+    );
+  };
+  
+
+export default Post;
--- src/Pages/myPage/myPage.tsx
+++ src/Pages/myPage/myPage.tsx
@@ -1,70 +1,80 @@
 import React from "react";
 import styled from "styled-components";
 import { ReactComponent as ModifyIcon } from "../../assets/images/icon-more-vertical.svg";
+import ProfileIcon from "../../assets/images/icon-user.png";
+import Post from "./component/post";
 
 const Container = styled.div`
-    max-height: calc(100vh - 60px);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
+  max-height: calc(100vh - 60px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  overflow: scroll;
+
+  -ms-overflow-style: none; /* IE, Edge */
+  scrollbar-width: none; /* Firefox */
+
+  &::-webkit-scrollbar {
+    display: none; /* Chrome, Safari */
+  }
 `;
 const MyPageArea = styled.div`
   width: 960px;
   height: 100%;
   display: flex;
   flex-direction: column;
-  align-items: center;   
-  border: 1px solid black;
-
+  align-items: center;
 `;
 const ProfileArea = styled.div`
   width: 100%;
   height: 200px;
-  border: 1px solid black;
   display: flex;
   align-items: center;
+  margin-bottom: 30px;
+  margin-top: 20px;
 `;
-const Photo = styled.div`
-    margin-left: 10%;
-    height: 120px;
-    width: 120px;
-    border: 1px solid black;
-    border-radius: 120px;
-
-`
+const Photo = styled.div<{ imageUrl: string }>`
+  margin-left: 10%;
+  height: 120px;
+  width: 120px;
+  border-radius: 120px;
+  background-image: url(${(props) => props.imageUrl}); 
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
 const Info = styled.div`
-    width: calc(75% - 120px);
-    padding-left: 20px;
-    height: 120px;
-    border: 1px solid black;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-`
+  width: calc(75% - 120px);
+  padding-left: 20px;
+  height: 120px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 const UserName = styled.div`
-    font-size: 40px;
-    font-weight: 500;
-`
+  font-size: 40px;
+  font-weight: 500;
+`;
 const Summary = styled.div`
-    color: #014421;
-    font-size: 24px;
-    font-weight: 700;
-`
+  color: #014421;
+  font-size: 24px;
+  font-weight: 700;
+`;
 const Modify = styled.div`
-    margin-right: 10%;
-    width: 5%;
-    height: 120px;
-    border: 1px solid black;    
-    display: flex;
-    justify-content: end;
-`
+  margin-right: 10%;
+  width: 5%;
+  height: 120px;
+  display: flex;
+  justify-content: end;
+`;
 const Bnt = styled.button`
-    height: 40px;
-    width: 40px;
-    border: none;
-    background-color: transparent;
-`
+margin-top: 5px;
+  height: 40px;
+  width: 40px;
+  border: none;
+  background-color: transparent;
+`;
 const PostArea = styled.div`
   width: 100%;
   display: flex;
@@ -72,40 +82,69 @@ const PostArea = styled.div`
   align-items: center;
 `;
 const Filter = styled.div`
-    width: 80%;
-    margin: 0 10% 0 10%;
-    border: 1px solid black;
-    height: 40px;
-`
+  width: 80%;
+  margin: 0 10% 0 10%;
+  height: 40px;
+`;
 const FilterBtn = styled.button`
-    height: 100%;
-    width: 100px;
-    border: none;
-    color:white;
-    background-color: #014421;
-    border-radius: 5px;
-`
+  height: 100%;
+  width: 100px;
+  border: none;
+  color: white;
+  background-color: #014421;
+  border-radius: 5px;
+`;
+const Posts = styled.div`
+  padding-top: 20px;
+  width: 80%;
+  min-height: 100px;
+`;
+
+const examplePost = {
+  bookTitle: "The Great Gatsby",
+  bookAuthor: "F. Scott Fitzgerald",
+  title: "A Classic Review",
+  text: "This is an amazing book!",
+  date: "2024-11-19",
+  author: "John Doe",
+  profilePhoto: "https://example.com/profile.jpg",
+  likes: 25,
+  comments: [
+    { author: "Jane Doe", text: "I love this book too!" },
+    { author: "Alex", text: "Great review!" },
+  ],
+};
+
 const MyPage: React.FC = () => {
   return (
     <Container>
       <MyPageArea>
         <ProfileArea>
-            <Photo></Photo>
-            <Info>
-                <UserName>asdf</UserName>
-                <Summary>asdf</Summary>
-            </Info>
-            <Modify>
-                <Bnt>
-                    <ModifyIcon/>
-                </Bnt>
-            </Modify>
+          <Photo imageUrl={ProfileIcon}></Photo>
+          <Info>
+            <UserName>asdf</UserName>
+            <Summary>asdf</Summary>
+          </Info>
+          <Modify>
+            <Bnt>
+              <ModifyIcon />
+            </Bnt>
+          </Modify>
         </ProfileArea>
         <PostArea>
-            <Filter>
-                <FilterBtn>asdf</FilterBtn>
-                <FilterBtn>asdf</FilterBtn>
-            </Filter>
+          <Filter>
+            <FilterBtn>asdf</FilterBtn>
+            <FilterBtn>asdf</FilterBtn>
+          </Filter>
+          <Posts>
+            <Post post={examplePost} />
+            <Post post={examplePost} />
+            <Post post={examplePost} />
+            <Post post={examplePost} />
+            <Post post={examplePost} />
+            <Post post={examplePost} />
+            <Post post={examplePost} />
+          </Posts>
         </PostArea>
       </MyPageArea>
     </Container>
